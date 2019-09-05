66a14179ac88c8391fb0650c39d5d3fbe8d6cd2f
mustatafiz012 <mustafiz012@outlook.com>
2019-09-06 01:32:20 +0600

    NavigationView, 'OnNavigationItemSelectedListener' callback added with expected actions.

--- app/src/main/java/com/musta/drawersample/MainActivity.java
+++ app/src/main/java/com/musta/drawersample/MainActivity.java
@@ -1,25 +1,29 @@
 package com.musta.drawersample;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-import android.support.design.widget.NavigationView;
-import android.support.v4.widget.DrawerLayout;
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-
-public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActivity";
     private AppBarConfiguration mAppBarConfiguration;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,9 +48,11 @@ public class MainActivity extends AppCompatActivity {
                 R.id.nav_tools, R.id.nav_share, R.id.nav_send)
                 .setDrawerLayout(drawer)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
@@ -61,5 +67,55 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        navController.popBackStack(R.id.nav_home, false);   //removing back stack except home fragment
+
+        switch (menuItem.getItemId()) {
+            case R.id.nav_home: {
+                navController.navigate(R.id.nav_home);
+                break;
+            }
+            case R.id.nav_gallery: {
+                navController.navigate(R.id.nav_gallery);
+                break;
+            }
+            case R.id.nav_slideshow: {
+                navController.navigate(R.id.nav_slideshow);
+                break;
+            }
+            case R.id.nav_tools: {
+                navController.navigate(R.id.nav_tools);
+                break;
+            }
+            case R.id.nav_share: {
+                navController.navigate(R.id.nav_share);
+                break;
+            }
+            case R.id.nav_send: {
+                navController.navigate(R.id.nav_send);
+                break;
+            }
+            case R.id.nav_logout: {
+                Toast.makeText(this, "Logging out", Toast.LENGTH_SHORT).show();
+                break;
+            }
+        }
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
